421667eb8d1a6a5ad1bf46d7d56c9a3dc0390bdb
tirtho.kyptronix <tirtho.kyptronix@gmail.com>
2025-02-14 17:20:01 +0530

    new

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
   socket.on("call-user", ({ userId, clientId }) => {
+    console.log(`User ${clientId} is calling user ${userId}`);
     if (activeCalls[userId]) {
+      console.log(`User ${userId} is busy. Notifying ${clientId}`);
       socket.emit("user-busy");
       return;
     }
@@ -28,33 +30,38 @@ io.on("connection", (socket) => {
     activeCalls[userId] = clientId;
     activeCalls[clientId] = userId;
 
+    console.log(`Notifying user ${userId} about incoming call from ${clientId}`);
     io.to(userId).emit("incoming-call", { from: clientId });
   });
 
   socket.on("accept-call", ({ userId, clientId }) => {
+    console.log(`User ${userId} accepted call from ${clientId}`);
     io.to(clientId).emit("call-accepted", { from: userId });
   });
 
   socket.on("offer", (data) => {
+    console.log(`Offer received from ${data.from} to ${data.to}`);
     io.to(data.to).emit("offer", { from: data.from, offer: data.offer });
   });
 
   socket.on("answer", (data) => {
+    console.log(`Answer received from ${data.from} to ${data.to}`);
     io.to(data.to).emit("answer", { from: data.from, answer: data.answer });
   });
 
   socket.on("ice-candidate", (data) => {
+    console.log(`ICE candidate received from ${data.from} to ${data.to}`);
     io.to(data.to).emit("ice-candidate", { from: data.from, candidate: data.candidate });
   });
 
   socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
     Object.keys(activeCalls).forEach((key) => {
       if (activeCalls[key] === socket.id) {
+        console.log(`Removing ${key} from active calls`);
         delete activeCalls[key];
       }
     });
-
-    console.log("User disconnected:", socket.id);
   });
 });
 
