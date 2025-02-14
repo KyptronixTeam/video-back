604b0c5a0670e08bbf99a1678705d7c72e017c03
tirtho.kyptronix <tirtho.kyptronix@gmail.com>
2025-02-14 15:55:30 +0530

    new

--- index.js
+++ index.js
@@ -13,19 +13,29 @@ const io = new Server(server, {
   },
 });
 
-let users = [];
+// Store users per room
+const rooms = {};
 
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
   socket.on("join-room", (roomId) => {
-    if (users.length >= 4) {
+    if (!rooms[roomId]) {
+      rooms[roomId] = [];
+    }
+
+    if (rooms[roomId].length >= 4) {
       socket.emit("room-full");
       return;
     }
-    users.push(socket.id);
+
+    rooms[roomId].push(socket.id);
     socket.join(roomId);
-    socket.emit("users", users.filter((id) => id !== socket.id));
+
+    // Send existing users to the newly joined user
+    socket.emit("users", rooms[roomId].filter((id) => id !== socket.id));
+
+    // Notify other users in the room
     socket.to(roomId).emit("user-joined", socket.id);
   });
 
@@ -42,8 +52,22 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
-    users = users.filter((id) => id !== socket.id);
-    io.emit("user-disconnected", socket.id);
+    let roomId = null;
+
+    // Find the user's room and remove them
+    for (const [key, users] of Object.entries(rooms)) {
+      if (users.includes(socket.id)) {
+        roomId = key;
+        rooms[key] = users.filter((id) => id !== socket.id);
+        break;
+      }
+    }
+
+    if (roomId) {
+      io.to(roomId).emit("user-disconnected", socket.id);
+      io.to(roomId).emit("users", rooms[roomId]); // Update remaining users
+    }
+
     console.log("User disconnected:", socket.id);
   });
 });
